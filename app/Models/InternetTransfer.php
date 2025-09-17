7cad7a51ff9d32f137482705f7ef7854b31b1a25
karamkharma <karamkharma6@gmail.com>
2025-09-17 22:12:05 +0300

    seconed  commit

--- app/Models/InternetTransfer.php
+++ app/Models/InternetTransfer.php
@@ -6,19 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class InternetTransfer extends Model
 {
-    protected $table = 'internet_transfers';
-
     protected $fillable = [
-        'receiver_number','quantity_gb','provider','type',
-        'status','confirmed_at','sms_hash','sms_meta','idempotency_key'
+        'sender_number','receiver_number','quantity_gb','price',
+        'provider','type','status','idempotency_key',
+        'confirmed_at','sms_hash','sms_meta',
     ];
 
     protected $casts = [
-        'quantity_gb' => 'decimal:3',
-        'price'       => 'decimal:2',
-        'quantity_gb' => 'float',
-        'confirmed_at'=> 'datetime',
-        'sms_meta'    => 'array',
-
+        'quantity_gb'  => 'float',
+        'confirmed_at' => 'datetime',
+        'sms_meta'     => 'array',
     ];
 }
