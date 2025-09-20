27d5d4f0bfa8a820db469c58c6066cd0b14479ad
karamkharma <karamkharma6@gmail.com>
2025-09-20 15:28:12 +0300

    seconed  commit

--- app/Models/UsdTransfer.php
+++ app/Models/UsdTransfer.php
@@ -9,16 +9,22 @@ class UsdTransfer extends Model
     protected $table = 'usd_transfers';
 
     protected $fillable = [
-    'sender_number','receiver_number','amount_usd','confirmed_amount_usd',
-    'confirmed_messages','fees','price','provider',
-    'exp_msg_3','exp_msg_2','exp_msg_1',
-];
-
+        'sender_number','receiver_number','amount_usd',
+        'confirmed_amount_usd','confirmed_messages','fees','price','provider',
+        'exp_msg_3','exp_msg_2_5','exp_msg_2','exp_msg_1_5','exp_msg_1','exp_msg_0_5',
+    ];
 
     protected $casts = [
-        'amount_usd' => 'decimal:2',
-        'confirmed_amount_usd' => 'decimal:2',
-        'fees' => 'decimal:4',
-        'price' => 'decimal:4',
+        'amount_usd'            => 'float',
+        'confirmed_amount_usd'  => 'float',
+        'fees'                  => 'float',
+        'price'                 => 'float',
+
+        'exp_msg_3'   => 'integer',
+        'exp_msg_2_5' => 'integer',
+        'exp_msg_2'   => 'integer',
+        'exp_msg_1_5' => 'integer',
+        'exp_msg_1'   => 'integer',
+        'exp_msg_0_5' => 'integer',
     ];
 }
