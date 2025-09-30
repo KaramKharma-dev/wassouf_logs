1edbf1d364a20fef04dc0dcec5e8f602f7318992
karamkharma <karamkharma6@gmail.com>
2025-09-30 19:17:37 +0300

    seconed  commit

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,11 @@ Route::post('/wish/process', [WishRowsProcessController::class, 'run'])->name('w
 Route::get('/wish-alt/process', [WishRawAltProcessController::class, 'index'])->name('wish.alt_process.index');
 Route::post('/wish-alt/process/run', [WishRawAltProcessController::class, 'run'])->name('wish.alt_process.run');
 
+// routes/web.php
+Route::get('/wish/pc', function () {
+    return view('wish.pc_upload');
+})->name('wish.pc');
+
 
 Route::get('/', function () {
     return view('welcome');
